9fa7f0c01fc341feea654802587c1ba363d503e9
rajiv-shrivastava <rjv834@gail.com>
2018-08-15 02:22:13 +0530

    ref:: file

---
+++ mystore_backend/app/controllers/products_controller.rb
@@ -0,0 +1,54 @@
+class ProductsController < ApplicationController
+  before_action :authenticate_user
+  before_action :set_product, only: [:show, :update, :destroy]
+
+  # GET /products
+  def index
+    if current_user.admin?
+      @products = product.all
+      render json: @products
+    end
+  end
+
+  # GET /products/1
+  def show
+    render json: @product
+  end
+
+  # POST /products
+  def create
+    @product = product.new(product_params)
+
+    if @product.save
+      render json: @product, status: :created, location: @product
+    else
+      render json: @product.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /products/1
+  def update
+    if @product.update(product_params)
+      render json: @product
+    else
+      render json: @product.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /products/1
+  def destroy
+    @product.destroy
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_product
+      @product = product.find(params[:id])
+    end
+
+    # Only allow a trusted parameter "white list" through.
+    def product_params
+      params.require(:product).permit(:name,:price,:margin) 
+    end
+
+end
--- mystore_backend/db/schema.rb
+++ mystore_backend/db/schema.rb
@@ -10,11 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170710132024) do
+ActiveRecord::Schema.define(version: 20180814204818) do
 
   create_table "bananas", force: :cascade do |t|
     t.string "name"
     t.string "location"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.integer "price"
+    t.integer "margin"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
